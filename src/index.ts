5c797e0cb812fd2fba2814efe7de5c13548e2340
David Werth <dw@omm-solutions.de>
2017-10-26 15:49:04 +0200

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {ModuleWithProviders, NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {NgxZxingComponent} from "./ngx-zxing.component";
+
+export * from "./ngx-zxing.component";
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        NgxZxingComponent,
+    ],
+    exports: [
+        NgxZxingComponent
+    ]
+})
+export class NgxZxingModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: NgxZxingModule
+        };
+    }
+}
---
+++ src/ngx-zxing.component.ts
@@ -0,0 +1,151 @@
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild} from "@angular/core";
+import {Subject} from "rxjs/Subject";
+import {BrowserQRCodeReaderExt} from "./browser-qr-code-reader-ext";
+
+@Component({
+    selector: "ngx-zxing",
+    template: `
+        <div class="form">
+            <video id="preview" #preview></video>
+        </div>`
+})
+export class NgxZxingComponent implements AfterViewInit, OnDestroy, OnChanges {
+
+    private destroyed$: Subject<any> = new Subject<any>();
+    private codeReader = new BrowserQRCodeReaderExt(1500);
+    private deviceId: string;
+
+    @ViewChild("preview")
+    previewElem: ElementRef;
+
+    @Input()
+    start = false;
+
+    @Input()
+    device: any;
+
+    @Output()
+    onScan = new EventEmitter<string>();
+
+    @Output()
+    onCamsFound = new EventEmitter<any[]>();
+
+    constructor() {
+        if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+            (<any>navigator).enumerateDevices = callback => {
+                navigator.mediaDevices.enumerateDevices().then(callback);
+            };
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.start) {
+            if (this.start) {
+                this.startCam();
+            } else {
+                this.stopCam();
+            }
+        }
+        if (changes.device && this.device) {
+            this.stopCam();
+            this.deviceId = this.device.deviceId;
+            if (this.start) {
+                this.startCam();
+            }
+        }
+    }
+
+    ngAfterViewInit() {
+        // iOS 11 Fix
+        this.previewElem.nativeElement.setAttribute("autoplay", true);
+        this.previewElem.nativeElement.setAttribute("muted", true);
+        this.previewElem.nativeElement.setAttribute("playsinline", true);
+        this.previewElem.nativeElement.setAttribute("autofocus", true);
+
+        this.enumerateCams();
+
+        if (this.start) {
+            this.startCam();
+        }
+    }
+
+    ngOnDestroy() {
+        this.destroyed$.next();
+        this.destroyed$.complete();
+    }
+
+    enumerateCams() {
+        navigator.mediaDevices.getUserMedia({audio: false, video: true}).then(
+            stream => {
+                this.getAllAudioVideoDevices(videoInputDevices => {
+                    if (videoInputDevices && videoInputDevices.length > 0) {
+                        this.onCamsFound.next(videoInputDevices);
+                        this.deviceId = videoInputDevices[0].deviceId;
+                    }
+                });
+                // Start stream so Browser can display permission-dialog ("Website wants to access your camera, allow?")
+                this.previewElem.nativeElement.srcObject = stream;
+                // After permission was granted, we can stop it again
+                stream.getVideoTracks().forEach(track => {
+                    track.stop();
+                });
+                stream.getAudioTracks().forEach(track => {
+                    track.stop();
+                });
+            }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    startCam() {
+        this.scan(this.deviceId);
+    }
+
+    scan(deviceId: string) {
+        this.codeReader.decodeFromInputVideoDevice(result => {
+            this.scanSuccess(result);
+        }, deviceId, this.previewElem.nativeElement);
+    }
+
+    stopCam() {
+        this.codeReader.reset();
+    }
+
+    scanSuccess(result: any) {
+        this.onScan.next(result.text);
+    }
+
+    getAllAudioVideoDevices(successCallback) {
+        if (!(<any>navigator).enumerateDevices) {
+            console.error("Can't enumerate Devices");
+            return;
+        }
+        const videoInputDevices = [];
+        (<any>navigator).enumerateDevices(devices => {
+            for (let i = 0, len = devices.length; i < len; i++) {
+                const device: any = {};
+                for (const d in devices[i]) {
+                    device[d] = devices[i][d];
+                }
+
+                if (device.kind === "video") {
+                    device.kind = "videoinput";
+                }
+                if (!device.deviceId) {
+                    device.deviceId = device.id;
+                }
+                if (!device.id) {
+                    device.id = device.deviceId;
+                }
+                if (!device.label) {
+                    device.label = "Camera (No Permission)";
+                }
+
+                if (device.kind === "videoinput" || device.kind === "video") {
+                    videoInputDevices.push(device);
+                }
+            }
+            successCallback(videoInputDevices);
+        });
+    }
+}
