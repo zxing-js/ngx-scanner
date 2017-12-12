56355b314ac6a6d2b5948466e148cb7803a0e883
Luiz Machado <machado@odahcam.com>
2017-12-12 17:18:35 -0200

    fixed public_api path

--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { NgxZxingModule } from '../../src/public_api';
+import { NgxZxingModule } from '../../public_api';
 
 import { AppComponent } from './app.component';
 
