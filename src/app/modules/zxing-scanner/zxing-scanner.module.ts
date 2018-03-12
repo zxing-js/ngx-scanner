8aeaafb595ab3ad3755817f34b26356e7ece76ee
David Werth <werth.david@gmail.com>
2018-03-12 08:34:56 +0100

    #23 Plunker Demo

--- src/app/modules/zxing-scanner/zxing-scanner.module.ts
+++ src/app/modules/zxing-scanner/zxing-scanner.module.ts
@@ -21,3 +21,26 @@ export class ZXingScannerModule {
         };
     }
 }
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ZXingScannerComponent } from './zxing-scanner.component';
+
+export type ZXingScannerComponent = ZXingScannerComponent;
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+    declarations: [ZXingScannerComponent],
+    exports: [ZXingScannerComponent],
+})
+export class ZXingScannerModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: ZXingScannerModule
+        };
+    }
+}
