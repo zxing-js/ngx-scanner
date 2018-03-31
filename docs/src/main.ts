a1ec0aff110600698c03efdd997fb149494cddc9
David Werth <werth.david@gmail.com>
2018-03-31 13:57:36 +0200

    Fixed Demo for Github.io

--- docs/src/main.ts
+++
@@ -1,7 +0,0 @@
-
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
